dbad1fcc4d6dfde51a1d0853dc156b238408ef8b
Vlad <vlad.mark-2013@mail.ru>
2020-06-19 13:49:08 +0300

    start

---
+++ samples/client/petstore/java/vertx/src/main/java/org/openapitools/client/api/PetApi.java
@@ -0,0 +1,33 @@
+package org.openapitools.client.api;
+
+import io.vertx.core.file.AsyncFile;
+import org.openapitools.client.model.ModelApiResponse;
+import org.openapitools.client.model.Pet;
+import java.util.Set;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+
+import java.util.*;
+
+public interface PetApi {
+
+    void addPet(Pet body, Handler<AsyncResult<Void>> handler);
+
+    void deletePet(Long petId, String apiKey, Handler<AsyncResult<Void>> handler);
+
+    void findPetsByStatus(List<String> status, Handler<AsyncResult<List<Pet>>> handler);
+
+    void findPetsByTags(Set<String> tags, Handler<AsyncResult<Set<Pet>>> handler);
+
+    void getPetById(Long petId, Handler<AsyncResult<Pet>> handler);
+
+    void updatePet(Pet body, Handler<AsyncResult<Void>> handler);
+
+    void updatePetWithForm(Long petId, String name, String status, Handler<AsyncResult<Void>> handler);
+
+    void uploadFile(Long petId, String additionalMetadata, AsyncFile file, Handler<AsyncResult<ModelApiResponse>> handler);
+
+    void uploadFileWithRequiredFile(Long petId, AsyncFile requiredFile, String additionalMetadata, Handler<AsyncResult<ModelApiResponse>> handler);
+
+}
